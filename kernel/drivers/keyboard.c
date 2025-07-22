6023d3461652a7923055ed3d5e3d17e1d0e655f6
Cian Payne <ciantheskier@gmail.com>
2025-07-22 22:06:00 +0900

    Mapped out a super-basic QWERTY layout

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -10,29 +10,28 @@ char keyboard_characters[] = {
     // Offset 0x02
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^',
     // TODO: handle backspace
-    '\0', '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '@', '[',
-    '\n',
+    0, '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '@', '[', '\n',
     // Offset 0x1e
     'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', ':',
     // Offset 0x2b
     ']', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
     // Offset 0x38
-    ' '};
+    '\\'};
 
 void initialize_keyboard() { return; }
 
 // TODO:
 char lookup_scancode(uint8_t scancode) {
-  if (scancode < 0x1e) {
+  if (scancode < 0x1E) {
     return keyboard_characters[scancode - 0x02];
-  } else if (scancode < 0x2b) {
-    return keyboard_characters[scancode - 0x1e];
+  } else if (scancode < 0x2B) {
+    return keyboard_characters[scancode - 0x03];
   } else if (scancode < 0x38) {
-    return keyboard_characters[scancode - 0x2b];
-  } else if (scancode == 0x38) {
-    return keyboard_characters[scancode];
+    return keyboard_characters[scancode - 0x05];
+  } else if (scancode == 0x39) {
+    return keyboard_characters[scancode - 0x07];
   } else {
-    return '\0';
+    return 0;
   }
 }
 
@@ -42,11 +41,7 @@ void handle_keyboard() {
   if (status & 0x1) {
     uint8_t scancode = inb(0x60);
     char character = lookup_scancode(scancode);
-    terminal_putchar(character);
-    // itoa(scancode, buf, 16);
-    // printf("Got scancode: ");
-    // printf(buf);
-    // printf("\n");
+    if (character != '\0') terminal_putchar(character);
   }
   send_end_of_interrupt(0x1);
 }
