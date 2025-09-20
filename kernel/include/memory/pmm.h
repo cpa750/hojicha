4df61ee782cb43fee55dbef873fe5adda4a8d823
Cian Payne <ciantheskier@gmail.com>
2025-09-21 00:00:09 +0900

    WIP: Put bitmap in page directly after kernel

--- kernel/include/memory/pmm.h
+++ kernel/include/memory/pmm.h
@@ -3,6 +3,6 @@
 
 void initialize_pmm(multiboot_info_t* m_info);
 void pmm_alloc_frame();
-void pmm_free_frame(uint16_t* page);
+void pmm_free_frame(uint32_t* page);
 void pmm_reserve_region(uint16_t idx, uint16_t len);
 
--- kernel/memory/pmm.c
+++ kernel/memory/pmm.c
@@ -1,7 +1,9 @@
 #include <drivers/serial.h>
 #include <limits.h>
 #include <memory/pmm.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define PAGE_SIZE 4096
 #define MEM_BITMAP_LENGTH 32768
@@ -11,11 +13,12 @@ uint32_t kernel_start = (uint32_t)&__kernel_start;
 extern uint32_t __kernel_end;
 uint32_t kernel_end = (uint32_t)&__kernel_end;
 
-// TODO: Align this to next page after kernel end,
-// reserve the region needed for it and memset to 0
 // 0 -> Free
 // 1 -> Reserved
-uint32_t mem_bitmap[32768] = {0};
+static uint8_t* mem_bitmap;
+
+uint32_t total_pages = 0;
+uint32_t free_pages = 0;
 
 void mark_page(uint32_t idx) {
   uint32_t mask = 1 << (idx & 31);
@@ -35,57 +38,37 @@ void pmm_reserve_region(uint16_t idx, uint16_t len) {
   // TODO figure out a more efficient way of doing this
   for (int i = 0; i < len; ++i) {
     mark_page(idx + i);
+    --free_pages;
   }
 }
 
 void initialize_pmm(multiboot_info_t* m_info) {
-  // Reserve low mem regions (up to 1M)
-  pmm_reserve_region(0, align_to_next_page(1048577));
   uint32_t max_section_length = 0;
   uint64_t max_section_start_addr = 0;
+  uint32_t available_mem_bytes = 0;
   for (uint32_t i = 0; i < m_info->mmap_length;
        i += sizeof(multiboot_memory_map_t)) {
     multiboot_memory_map_t* mmap_entry =
         (multiboot_memory_map_t*)(m_info->mmap_addr + i);
+    available_mem_bytes += mmap_entry->len;
     if (mmap_entry->len > max_section_length &&
         mmap_entry->type == MULTIBOOT_MEMORY_AVAILABLE) {
       max_section_length = mmap_entry->len;
       max_section_start_addr = mmap_entry->addr;
     }
   }
+
+  uint32_t mem_bitmap_bytes = sizeof(uint32_t) * MEM_BITMAP_LENGTH;
+  if (max_section_length > (kernel_end - kernel_start + mem_bitmap_bytes)) {
+    printf("Not enough memory to load memory bitmap. Halt.");
+    abort();
+  }
+
+  total_pages = max_section_length;
+  mem_bitmap = (uint8_t*)((align_to_next_page(kernel_end + 1) + 1) * PAGE_SIZE);
+  memset(mem_bitmap, 0xFF, mem_bitmap_bytes);
+
   uint16_t kernel_start_page = align_to_prev_page(kernel_start);
   uint16_t kernel_end_page = align_to_next_page(kernel_end + 1);
-  pmm_reserve_region(kernel_start_page, kernel_end_page - kernel_start_page);
-
-  char buf[100];
-  itoa(kernel_start, buf, 16);
-  serial_write_string("kernel_start: ");
-  serial_write_string(buf);
-  serial_write_char('\n');
-
-  itoa(kernel_end, buf, 16);
-  serial_write_string("kernel_end: ");
-  serial_write_string(buf);
-  serial_write_char('\n');
-
-  itoa(align_to_prev_page(kernel_start), buf, 16);
-  serial_write_string("kernel_start Page: ");
-  serial_write_string(buf);
-  serial_write_char('\n');
-
-  itoa(align_to_next_page(kernel_end + 1), buf, 16);
-  serial_write_string("kernel_end Page: ");
-  serial_write_string(buf);
-  serial_write_char('\n');
-
-  itoa(kernel_end - kernel_start + 1, buf, 16);
-  serial_write_string("kernel size: ");
-  serial_write_string(buf);
-  serial_write_char('\n');
-
-  itoa((uint32_t)mem_bitmap, buf, 16);
-  serial_write_string("mem_bitmap addr: ");
-  serial_write_string(buf);
-  serial_write_char('\n');
 }
 
