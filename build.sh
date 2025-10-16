30996ccbdc507fe2e2a5ae75a8e831a82c5b7929
Cian Payne <ciantheskier@gmail.com>
2025-10-16 23:15:45 +0900

    WIP start work on x86_64 boot with Limine

--- build.sh
+++ build.sh
@@ -7,6 +7,10 @@ if [[ "$*" == *"--debug-qemu"* ]]; then
     DEBUG_QEMU="DEBUG_QEMU=1"
 fi
 
+if [[ "$*" == *"--h64"* ]]; then
+    HOST=x86_64
+fi
+
 for PROJECT in $PROJECTS; do
-  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE $DEBUG_QEMU install)
+  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE $DEBUG_QEMU HOST=$HOST install)
 done
