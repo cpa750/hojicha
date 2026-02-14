296d5ff7959d123827d86d97c53da61fa1a65a21
Cian Payne <ciantheskier@gmail.com>
2026-02-14 20:48:21 +0900

    Add log level parameter to build/run scripts

--- build.sh
+++ build.sh
@@ -15,8 +15,39 @@ if [[ "$*" == *"--kmalloc-test"* ]]; then
     KMALLOC_TEST="KMALLOC_TEST=1"
 fi
 
+HLOG_LEVEL=" "
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --hlog-level=*)
+      HLOG_LEVEL_VALUE="${1#*=}"
+      ;;
+    --hlog-level)
+      shift
+      if [ -z "$1" ]; then
+        echo "--hlog-level requires a value" >&2
+        exit 1
+      fi
+      HLOG_LEVEL_VALUE="$1"
+      ;;
+  esac
+  shift
+done
+
+if [ -n "$HLOG_LEVEL_VALUE" ]; then
+  case "$HLOG_LEVEL_VALUE" in
+    FATAL|ERROR|WARN|INFO|DEBUG|VERBOSE)
+      HLOG_LEVEL="HLOG_LEVEL=HLOG_$HLOG_LEVEL_VALUE"
+      ;;
+    *)
+      echo "Unknown --hlog-level value: $HLOG_LEVEL_VALUE" >&2
+      echo "Valid values: FATAL, ERROR, WARN, INFO, DEBUG, VERBOSE" >&2
+      exit 1
+      ;;
+  esac
+fi
+
 for PROJECT in $PROJECTS; do
-  (cd $PROJECT && DESTDIR="$SYSROOT" bear --append -- $MAKE $DEBUG_QEMU $KMALLOC_TEST HOST=$HOST install)
+  (cd $PROJECT && DESTDIR="$SYSROOT" bear --append -- $MAKE $DEBUG_QEMU $KMALLOC_TEST $HLOG_LEVEL HOST=$HOST install)
 done
 
 jq -s 'add' $(printf '%s/compile_commands.json ' $PROJECTS) > compile_commands.json
