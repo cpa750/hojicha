808bfbef4fc053dceed27ece76d006cef28a1d63
Cian Payne <ciantheskier@gmail.com>
2026-06-03 21:28:15 +0900

    Add CMake debug configuration

--- headers.sh
+++ headers.sh
@@ -7,12 +7,16 @@ mkdir -p "$SYSROOT"
 HOSTARCH=$(./target_triplet_to_arch.sh "$HOST")
 BUILDDIR="build/$HOSTARCH/headers"
 
-"$CMAKE" -S . -B "$BUILDDIR" \
-  -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/elf.cmake \
-  -DHOJICHA_HOST="$HOST" \
-  -DHOJICHA_ARCH="$HOSTARCH" \
-  -DHOJICHA_SYSROOT="$SYSROOT" \
-  -DHOJICHA_BUILD_FLAVOR=headers \
-  -DCMAKE_INSTALL_PREFIX=/usr
+echo "Calling CMake:"
+(
+  set -x
+  "$CMAKE" -S . -B "$BUILDDIR" \
+    -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/elf.cmake \
+    -DHOJICHA_HOST="$HOST" \
+    -DHOJICHA_ARCH="$HOSTARCH" \
+    -DHOJICHA_SYSROOT="$SYSROOT" \
+    -DHOJICHA_BUILD_FLAVOR=headers \
+    -DCMAKE_INSTALL_PREFIX=/usr
+)
 
 DESTDIR="$SYSROOT" "$CMAKE" --install "$BUILDDIR" --component headers
