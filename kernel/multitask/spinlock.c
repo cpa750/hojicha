e20444c8576b768ec080f495ee07a66de446727f
Cian Payne <ciantheskier@gmail.com>
2026-06-13 19:02:31 +0900

    PMM alloc and free now behind spinlock

--- kernel/multitask/spinlock.c
+++ kernel/multitask/spinlock.c
@@ -8,6 +8,7 @@ void spinlock_init(spinlock_t* lock) {
 }
 
 uint64_t spinlock_lock(spinlock_t* lock) {
+  if (lock == NULL) { return 0; }
   uint64_t irq_state = irq_store();
   while (__sync_lock_test_and_set(&lock->locked, 1)) {}
   return irq_state;
